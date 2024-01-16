8aefd2c63e13d8a3f9082c39332bc4a66287d3e4
Priyadarsh S S <priyadarsh2001@gmail.com>
2024-01-16 23:20:38 +0530

    feat: user chips component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,174 @@
+'use client'
 import Image from 'next/image'
+import { useState, useEffect, useRef } from 'react'
+import { data } from '../data'
 
-export default function Home() {
+interface ListData {
+  id: number
+  name: string
+  email: string
+  avatar: string
+}
+
+export default function ChipComponent() {
+  const [inputValue, setInputValue] = useState<string>('')
+  const [chipData, setChipData] = useState<ListData[]>([])
+  const [listData, setListData] = useState<ListData[]>(data)
+  const [showDropdown, setShowDropdown] = useState<boolean>(false)
+  const inputRef = useRef<HTMLInputElement>(null)
+  const dropdownRef = useRef<HTMLInputElement>(null)
+  const [backspaceCount, setBackspaceCount] = useState(0)
+  const [highlightedChipIndex, setHighlightedChipIndex] = useState<number>(-1)
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (
+        inputRef.current &&
+        !inputRef.current.contains(event.target as Node) &&
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node)
+      ) {
+        setShowDropdown(false)
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside)
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [inputRef, dropdownRef])
+
+  useEffect(() => {
+    function handleKeyDown(event: KeyboardEvent) {
+      if (
+        event.key === 'Backspace' &&
+        inputValue === '' &&
+        chipData.length > 0
+      ) {
+        const lastIndex = chipData.length - 1
+        setHighlightedChipIndex(lastIndex)
+        setBackspaceCount((prevCount) => prevCount + 1)
+      }
+    }
+
+    if (chipData.length === 0) setHighlightedChipIndex(-1)
+
+    document.addEventListener('keydown', handleKeyDown)
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown)
+    }
+  }, [inputValue, chipData])
+
+  useEffect(() => {
+    if (backspaceCount === 2) {
+      const lastIndex = chipData.length - 1
+      setListData([...listData, chipData[lastIndex]])
+      setChipData(chipData.filter((_, index) => index !== lastIndex))
+      setBackspaceCount(0)
+    }
+  }, [backspaceCount])
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value)
+  }
+
+  const handleItemClick = (item: ListData) => {
+    setChipData([...chipData, item])
+    setListData((prevListData) =>
+      prevListData.filter((data) => data.name !== item.name)
+    )
+    setInputValue('')
+  }
+
+  const handleDeleteChip = (chip: ListData) => {
+    setChipData(chipData.filter((data) => data.name !== chip.name))
+    setListData([...listData, chip])
+
+    if (highlightedChipIndex === chipData.length - 1) {
+      setHighlightedChipIndex(-1)
+    }
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className='min-h-screen flex flex-col pt-60 items-center p-4 w-full'>
+      <div className='relative mb-4 w-2/3 flex'>
+        {/* chip div */}
+        <div
+          className={
+            'flex border-2 border-b-blue-800 border-t-0 border-l-0 border-r-0 ' +
+            `${chipData.length > 3 ? 'min-w-[70%] flex-wrap' : ''}`
+          }
+        >
+          {chipData.map((chip, index) => (
+            <span
+              ref={inputRef}
+              key={chip.name}
+              className={`bg-[#cbcbcb] text-black m-1 my-2 w-max rounded-full inline-flex items-center ${
+                index === highlightedChipIndex ? 'border-2 border-blue-500' : ''
+              }`}
+              onClick={() => setHighlightedChipIndex(index)}
+            >
+              <Image
+                src={chip.avatar}
+                alt={chip.name}
+                className='w-8 h-8 rounded-full mr-2'
+                width={1000}
+                height={1000}
+              />
+              <span>{chip.name}</span>
+              <button
+                onClick={() => handleDeleteChip(chip)}
+                className='text-black font-semibold cursor-pointer'
+              >
+                <span className='text-xl align-center px-2'>&times;</span>
+              </button>
+            </span>
+          ))}
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+        {/* input */}
+        <input
+          type='text'
+          value={inputValue}
+          onChange={handleInputChange}
+          onClick={() => setShowDropdown(true)}
+          placeholder='Add new user...'
+          className='border-2 border-t-0 border-l-0 border-r-0 p-2 w-full bg-inherit border-b-blue-800 focus:outline-none '
+          ref={inputRef}
         />
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+      {/* dropdown mapping */}
+      {(showDropdown || inputValue.length > 0) && (
+        <div
+          ref={dropdownRef}
+          className='max-h-[300px] overflow-auto bg-white shadow-gray-400 shadow-lg rounded-lg'
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          {listData
+            .filter((item) =>
+              item.name.toLowerCase().includes(inputValue.toLowerCase())
+            )
+            .map((filteredItem) => (
+              <div
+                key={filteredItem.id}
+                onClick={() => handleItemClick(filteredItem)}
+                className='border-b p-3 cursor-pointer hover:bg-gray-200 flex items-center'
+              >
+                <Image
+                  src={filteredItem.avatar}
+                  alt={filteredItem.name}
+                  className='w-12 h-12 rounded-full mr-2'
+                  width={1000}
+                  height={1000}
+                />
+                <div className='flex w-full justify-between items-center gap-4'>
+                  <div className='font-semibold'>{filteredItem.name}</div>
+                  <div className='text-sm text-gray-500'>
+                    {filteredItem.email}
+                  </div>
+                </div>
+              </div>
+            ))}
+        </div>
+      )}
+    </div>
   )
 }
---
+++ src/data/index.js
@@ -0,0 +1,44 @@
+export const data = [
+    {
+      id: 1,
+      name: 'Ken Alvarez',
+      email: 'ken.alvarez@example.com',
+      avatar:
+        'https://images.pexels.com/photos/428328/pexels-photo-428328.jpeg?auto=compress&cs=tinysrgb&w=600',
+    },
+    {
+      id: 2,
+      name: 'Tracy Bing',
+      email: 'tracy.bing@example.com',
+      avatar:
+        'https://images.pexels.com/photos/1520760/pexels-photo-1520760.jpeg?auto=compress&cs=tinysrgb&w=600',
+    },
+    {
+      id: 3,
+      name: 'Soham Newman',
+      email: 'soham.newman@example.com',
+      avatar:
+        'https://images.pexels.com/photos/3190334/pexels-photo-3190334.jpeg?auto=compress&cs=tinysrgb&w=600',
+    },
+    {
+      id: 4,
+      name: 'Denise Hall',
+      email: 'denise.hall@example.com',
+      avatar:
+        'https://images.pexels.com/photos/3936894/pexels-photo-3936894.jpeg?auto=compress&cs=tinysrgb&w=600',
+    },
+    {
+      id: 5,
+      name: 'Letitia George',
+      email: 'letitia.george@example.com',
+      avatar:
+        'https://images.pexels.com/photos/751235/pexels-photo-751235.jpeg?auto=compress&cs=tinysrgb&w=600',
+    },
+    {
+      id: 6,
+      name: 'Calvin Jones',
+      email: 'calvin.jones@example.com',
+      avatar:
+        'https://images.pexels.com/photos/2537658/pexels-photo-2537658.jpeg?auto=compress&cs=tinysrgb&w=600',
+    },
+  ]
